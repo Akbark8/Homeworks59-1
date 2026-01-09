bac4014f51afe52cbe839b48ca6c80190d833868
Akbark8 <konokovakbar@gmail.com>
2026-01-09 16:57:35 +0600

    homework4

--- shop_api/product/serializers.py
+++ shop_api/product/serializers.py
@@ -3,11 +3,18 @@ from .models import Category, Product, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    products_count = serializers.IntegerField(read_only=True)
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Название категории должно быть не менее 3 символов"
+            )
+        return value
 
     class Meta:
         model = Category
-        fields = ('id', 'name', 'products_count')
+        fields = "__all__"
+
 
 
 
@@ -37,3 +44,43 @@ class ProductReviewSerializer(serializers.ModelSerializer):
 
     def get_rating(self, obj):
         return obj.reviews.aggregate(avg=Avg('stars'))['avg']
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Название товара должно быть не менее 3 символов"
+            )
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Цена должна быть больше 0"
+            )
+        return value
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    def validate_text(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError(
+                "Отзыв должен содержать минимум 5 символов"
+            )
+        return value
+
+    def validate_stars(self, value):
+        if not 1 <= value <= 5:
+            raise serializers.ValidationError(
+                "Рейтинг должен быть от 1 до 5"
+            )
+        return value
+
+    class Meta:
+        model = Review
+        fields = "__all__"
